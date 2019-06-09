dd49c94113db4c9be14f772f3a2bbaeff31da76b
xkrik <krikonis.chr@gmail.com>
2019-06-09 03:47:35 +0300

    Plus Button Added

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -2,31 +2,55 @@ import React, { Component } from 'react'
 import Player from './Player'
 import './Scoreboard.css'
 
-const players = [
-  {
-    id: 1,
-    name: 'Makis',
-    score: 4
-  },
-  {
-    id: 2,
-    name: 'Demi',
-    score: 5
-  },
-  {
-    id: 3,
-    name: 'Tasos',
-    score: 3
-  }
-]
-
 export default class Scoreboard extends Component {
-  renderPlayer(player) {
-    return <Player key={player.id}
-      name={player.name}
-      score={player.score}
-    />
-  }
+    state = {
+        players: [
+            {
+             id: 1,
+             name: 'Makis',
+             score: 4
+            },
+            {
+             id: 2,
+             name: 'Demi',
+             score: 5
+            },
+            {
+             id: 3,
+             name: 'Tasos',
+             score: 3
+            }
+        ]
+    }
+
+    updatePlayerScore = (id, score) => {
+        const updatedPlayers = this.state.players.map(
+          player => {
+            if (player.id === id) {
+              return {
+                ...player,
+                score
+              }
+            }
+            else {
+              return player
+            }
+          }
+        )
+        this.setState({ players: updatedPlayers })
+      }
+
+    renderPlayer = (player) => {
+    return (
+        <Player
+          id={player.id}
+          key={player.id}
+          name={player.name}
+          score={player.score}
+          updatePlayerScore={this.updatePlayerScore}
+        />
+    )
+    } 
 
   render() {
     return (
@@ -34,7 +58,7 @@ export default class Scoreboard extends Component {
         <h1>Scoreboard</h1>
         <ul>
           {
-            players
+            this.state.players
               .sort((a, b) => b.score - a.score)
               .map(this.renderPlayer)
           }
